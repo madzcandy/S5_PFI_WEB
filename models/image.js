964d336e114c4c636f2357aec092b3cf654a3ecc
MightyActarus <vlaverdure@hotmail.ca>
2022-11-22 09:13:59 -0500

    images model

--- models/image.js
+++ models/image.js
@@ -6,10 +6,13 @@ class Image extends Model{
         super();
         this.Title = "";
         this.Description = "";
-        this.Date =  0;
+        this.Date = 0;
+        this.UserId = 0;
+        this.Shared = false;
         this.GUID = "";
 
         this.addValidator('Title','string');
         this.addValidator('Description', 'string');
+        this.addValidator('Shared', 'boolean');
     }
 }
