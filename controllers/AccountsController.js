06fcddb038006323a33eb88ec48670b36dc52ad9
MightyActarus <vlaverdure@hotmail.ca>
2022-12-03 00:36:10 -0500

    mini fix par rapport à la rencontre de ce matin

--- controllers/AccountsController.js
+++ controllers/AccountsController.js
@@ -113,7 +113,7 @@ module.exports =
                     let updateResult = this.repository.update(user);
                     if (updateResult == this.repository.updateResult.ok) {
                         this.HttpContext.response.ok();
-                        if (user.Email != foundedUser.Email) {
+                        if (user.Email != foundUser.Email) {
                             user.VerifyCode = utilities.makeVerifyCode(6);
                             this.repository.update(user);
                             this.sendVerificationEmail(user);
--- wwwroot/ImagesManager2022/js/api.js
+++ wwwroot/ImagesManager2022/js/api.js
@@ -213,16 +213,15 @@ function VERIFY_EMAIL(userId, verifyCode, successCallBack, errorCallBack) {
 // }
 function MODIFY_USER_INFO(userInfo, successCallBack, errorCallBack) {
     $.ajax({
-        url: server + "/accounts/modify",
+        url: server + "/accounts/modify/" + userInfo.Id,
         type: 'PUT',
         contentType: 'application/json',
         headers: {
             authorization: 'Bearer '+ retrieveAccessToken()
         },
         data: JSON.parse(userInfo),
-        success: function (status) {
-            successCallBack(status);
-            GETUSERINFO(userId, successCallBack, error);
+        success: function () {
+            GETUSERINFO(userInfo.Id, successCallBack, error);
         },
         error: function (jqXHR) { errorCallBack(jqXHR.status) }
     })
